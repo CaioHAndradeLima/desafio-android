921f9670a90d86cdf3435a4114e875e88b48104c
araujoraul <raulespim.p@gmail.com>
2020-04-15 00:01:06 -0300

    adapter implemented

--- app/src/main/java/com/araujoraul/desafioandroid/presentation/ui/reposlist/ReposListAdapter.kt
+++ app/src/main/java/com/araujoraul/desafioandroid/presentation/ui/reposlist/ReposListAdapter.kt
@@ -4,45 +4,65 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.araujoraul.desafioandroid.R
 import com.araujoraul.desafioandroid.data.model.Repository
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_repos_list.view.*
 
-class ReposListAdapter(var list: List<Repository>) :
-       RecyclerView.Adapter<ReposListAdapter.ReposListViewHolder>()
-{
+class ReposListAdapter : ListAdapter<Repository, ReposListAdapter.ReposListViewHolder>(REPO_COMPARATOR) {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReposListViewHolder {
+
         return ReposListViewHolder(LayoutInflater.from(parent.context)
                 .inflate(R.layout.item_repos_list, parent, false))
     }
-
-    override fun getItemCount(): Int = list.count()
-
     override fun onBindViewHolder(holder: ReposListViewHolder, position: Int) {
-        holder.bind(list[position])
+
+       val item = getItem(position)
+
+        if (item != null){
+           holder.bind(item)
+        }
     }
 
     class ReposListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
-        fun bind(item: Repository?){
-            item?.let {
+        private var repo: Repository? = null
+
+        fun bind(repo: Repository?){
+
+            repo?.let {
 
                 with(itemView){
-                    txtName.text = item.name
-                    txtDescription.text = item.description
-                    forks.text = item.forksCount.toString()
-                    stars.text = item.stargazersCount.toString()
-                    username.text = item.owner.login
+                    txtName.text = repo.name
+                    txtDescription.text = repo.description
+                    forks.text = repo.forksCount.toString()
+                    stars.text = repo.stargazersCount.toString()
+                    username.text = repo.owner.login
 
-                    avatar.loadImage(item.owner.avatarUrl)
+                    avatar.loadImage(repo.owner.avatarUrl)
                 }
             }
         }
+
+        private fun showRepoData() {
+          TODO( "Not implemented yet")
+        }
+
     }
 
+    companion object{
+
+        private val REPO_COMPARATOR = object : DiffUtil.ItemCallback<Repository>() {
+            override fun areItemsTheSame(oldItem: Repository, newItem: Repository) = oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Repository, newItem: Repository) = oldItem.id == newItem.id
+        }
+    }
 
 }
 
